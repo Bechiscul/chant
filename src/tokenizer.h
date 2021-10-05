c96c2d0c90c8c38a99f682e2af370f9cb6164a0c
Bech1337 <sebberas2@gmail.com>
2021-10-05 14:35:27 +0200

    Update token types to only be chars

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -3,50 +3,35 @@
 #include <stdint.h>
 #include <string.h>
 
+/// The type of the token.
 typedef enum TokenType {
-  TK_UNKNOWN = -1,
-  TK_SYMBOL,
-  TK_NUMBER,
-  TK_STRING,
-  TK_ASSIGN,
-  TK_COLON,
-  TK_SEMICOLON,
-  TK_ADD,
-  TK_SUB,
-  TK_ASTERISK,
-  TK_SLASH,
-  TK_MODOLUS,
-  TK_POW,
-  TK_LPAREN,
-  TK_RPAREN,
-  TK_ADD_ASSIGN,
-  TK_SUB_ASSIGN,
-  TK_MUL_ASSIGN,
-  TK_DIV_ASSIGN,
-  TK_MOD_ASSIGN,
-  TK_BIT_XOR,
-  TK_BIT_XOR_ASSIGN,
-  TK_LBIT_SHIFT,
-  TK_RBIT_SHIFT,
-  TK_BIT_OR_ASSIGN,
-  TK_LBIT_SHIFT_ASSIGN,
-  TK_RBIT_SHIFT_ASSIGN,
-  TK_NOT,
-  TK_EQ,
-  TK_NOT_EQ,
-  TK_AND,
-  TK_OR,
-  TK_ARROW,
-  TK_GREATER,
-  TK_LESSER,
-  TK_GREATER_EQ,
-  TK_LESSER_EQ,
+  TK_UNKNOWN = -1, //< Unkown token.
+  TK_SYMBOL,       //< A symbol.
+  TK_NUMBER,       //< A number. Eg. 123 or 1.2351
+  TK_STRING,       //< A string. Eg. "hello world"
+  TK_EQUAL,        //< =
+  TK_COLON,        //< :
+  TK_SEMICOLON,    //< ;
+  TK_PLUS,         //< +
+  TK_MINUS,        //< -
+  TK_ASTERISK,     //< *
+  TK_SLASH,        //< /
+  TK_PERCENT,      //< %
+  TK_CARET,        //< ^
+  TK_LPAREN,       //< (
+  TK_RPAREN,       //< )
+  TK_LBRACE,       //< {
+  TK_RBRACE,       //< }
+  TK_QS,           //< !
+  TK_AND,          //< &
+  TK_GREAT,        //< >
+  TK_LESS,         //< <
 
-  MAX_TOKENS,
+  // The amount of different valid tokens. NOTE Doesn't include TK_UNKNOWN.
+  MAX_TOKEN
 } TokenType;
 
 //"fn_call(x, y)" => ["fn_call", "(", "x", ",", "y", ")"]
-
 typedef struct Token {
   TokenType type;
   char *string;
