aeb4b52746bfa4bfe1c50a9286592c88148f1404
unic0rn9k <unic0rn9k@protonmail.com>
2021-10-06 20:03:18 +0200

    Better values for TokenType variants, and some tokenizer stuff

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -2,20 +2,60 @@
 
 #include "tokenizer.h"
 
-bool str_contains(const char *source, char c) {
-  for (int n = 0; n < strlen(source); n++) {
-    if (source[n] == c) {
-      return true;
-    }
-  }
-  return false;
-}
+const char *SPLIT_AT = "()[],{}.!*=-+/%^&>|<;: ";
 
-const char *SPLIT_AT = "()[],{}.!*=-+/%^&>|<;:";
-
-size_t scanTokens(const char *source, Token *buffer, size_t buffer_size) {
+size_t countTokens(const char *source) {
   size_t tokens = 0;
   for (int n = 0; n < strlen(source); n++) {
+    if (strchr(SPLIT_AT, source[n]))
+      tokens++;
+  }
+  return tokens;
+}
+
+Token stringToToken(const char *source) {
+  Token token;
+  token.type = TK_UNKNOWN;
+  switch (source[0]) {
+
+  case '(':
+    token.type = TK_LPAREN;
+    break;
+
+  case ')':
+    token.type = TK_RPAREN;
+    break;
+
+  case '{':
+    token.type = TK_LBRACE;
+    break;
+  }
+
+  return token;
+}
+
+/// Source string should never contain new lines,
+/// tabs or more than one consecutive spaces.
+size_t scanTokens(const char *source, Token *buffer, size_t buffer_size) {
+  if (buffer == NULL)
+    return countTokens(source);
+
+  // char *last_token = source[0..3];
+
+  size_t tokens = 0;
+  size_t token_start = 0;
+  size_t token_len = 0;
+
+  for (int n = 0; n < strlen(source); n++) {
+
+    if (strchr(SPLIT_AT, source[n])) {
+      // use stringToToken and strncpy(result, str + start, end - start);
+
+      tokens++;
+      token_start = n + 1;
+      token_len = 0;
+    }
+    token_len++;
   }
   return tokens;
 }
--- src/tokenizer.h
+++ src/tokenizer.h
@@ -5,27 +5,29 @@
 
 /// The type of the token.
 typedef enum TokenType {
-  TK_UNKNOWN = -1, //< Unkown token.
-  TK_SYMBOL,       //< A symbol.
-  TK_NUMBER,       //< A number. Eg. 123 or 1.2351
-  TK_STRING,       //< A string. Eg. "hello world"
-  TK_EQUAL,        //< =
-  TK_COLON,        //< :
-  TK_SEMICOLON,    //< ;
-  TK_PLUS,         //< +
-  TK_MINUS,        //< -
-  TK_ASTERISK,     //< *
-  TK_SLASH,        //< /
-  TK_PERCENT,      //< %
-  TK_CARET,        //< ^
-  TK_LPAREN,       //< (
-  TK_RPAREN,       //< )
-  TK_LBRACE,       //< {
-  TK_RBRACE,       //< }
-  TK_QS,           //< !
-  TK_AND,          //< &
-  TK_GREAT,        //< >
-  TK_LESS,         //< <
+  TK_UNKNOWN = -1,    //< Unkown token.
+  TK_SYMBOL = 128,    //< A symbol.
+  TK_NUMBER = 129,    //< A number. Eg. 123 or 1.2351
+  TK_STRING = 130,    //< A string. Eg. "hello world"
+  TK_EQUAL = '=',     //< =
+  TK_COLON = ':',     //< :
+  TK_SEMICOLON = ';', //< ;
+  TK_PLUS = '+',      //< +
+  TK_MINUS = '-',     //< -
+  TK_ASTERISK = '*',  //< *
+  TK_SLASH = '/',     //< /
+  TK_PERCENT = '%',   //< %
+  TK_CARET = '^',     //< ^
+  TK_LPAREN = '(',    //< (
+  TK_RPAREN = ')',    //< )
+  TK_LBRACE = '{',    //< {
+  TK_RBRACE = '}',    //< }
+  TK_LSQBRACE = '[',  //< [
+  TK_RSQBRACE = ']',  //< ]
+  TK_EP = '!',        //< !
+  TK_AND = '&',       //< &
+  TK_GREAT = '>',     //< >
+  TK_LESS = '<',      //< <
 
   // The amount of different valid tokens. NOTE Doesn't include TK_UNKNOWN.
   MAX_TOKEN
