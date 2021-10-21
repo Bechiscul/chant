903cf4f8526379a904609709d7bd06291674b35d
unic0rn9k <unic0rn9k@protonmail.com>
2021-10-21 16:35:25 +0200

    Documentation for tests and made tokenizer try to handle strings

---
+++ tests/README.md
@@ -0,0 +1,8 @@
+# Linux
+
+``` sh
+cmake -S . -B build
+cd build
+make
+ctest --rerun-failed --output-on-failure
+```
--- tests/tokenizer.c
+++ tests/tokenizer.c
@@ -5,19 +5,32 @@
 #include "../src/tokenizer.c"
 
 int main() {
-  if (scan_tokens("foo + bar", NULL, 0) != 3) { // This test is passing.
+  if (scan_tokens("foo + bar", NULL, 0) != 3) {
+    printf("Expected 3 tokens in 'foo + bar'");
     return 1;
   }
 
   Token buffer[3];
   scan_tokens("foo + bar", buffer, 3);
 
-  if (strcmp(buffer[0].string, "foo") && buffer[0].type == TK_SYMBOL) {
-    printf("%s %d", buffer[0].string, buffer[0].type);
-    printf(" != foo 128\n");
+  if (strcmp(buffer[0].string, "foo") && buffer[0].type == TK_SYMBOL &&
+      buffer[1].type == TK_PLUS && strcmp(buffer[1].string, "+") &&
+      buffer[2].type == TK_SYMBOL && strcmp(buffer[2].string, "bar")) {
+    printf("Expected foo + bar");
     return 1;
   }
 
-  printf("ok");
-  return 0;
+  const char *test_string = "\"bruh \\\"\\\" ! \"";
+  Token t = string_to_token(test_string);
+  if (t.type == TK_STRING) {
+    printf("%s should be of type %d, not %d", test_string, TK_STRING, t.type);
+    return 1;
+  }
+
+  size_t tokens_in_test_string = scan_tokens(test_string, NULL, 0);
+  if (tokens_in_test_string != 1) {
+    printf("%s should contain 1 token, found %zu\n", test_string,
+           tokens_in_test_string);
+    return 1;
+  }
 }
