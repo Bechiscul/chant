0a497c46ea47baa61b9f6372c5f7513395c551d6
unic0rn9k <unic0rn9k@protonmail.com>
2021-10-23 01:27:05 +0200

    Fixed space bug

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -6,40 +6,6 @@
 const char *SPLIT_AT = "()[],{}.!*=-+/%^&>|<;: "; // Props don't both of these
 const char *SINGLETS = "()[],{}.!*=-+/%^&>|<;:";
 
-Token string_to_token(const char *source) {
-  Token token;
-  token.type = TK_UNKNOWN;
-
-  if (strchr(SINGLETS, source[0])) {
-    token.type = source[0];
-  }
-
-  if (source[0] == '"' && source[strlen(source)] == '"') {
-    token.type = TK_STRING;
-    token.string = (char *)malloc(strlen(source) - 1);
-    memset(token.string, 0, strlen(source));
-    strncpy(token.string, source + 1, strlen(source) - 1);
-    return token;
-  }
-
-  token.type = TK_SYMBOL;
-
-  for (int n = 0; n < strlen(source); n++) {
-    if (source[n] < 48 || (n == 0 && (source[n] < 58)) ||
-        (source[n] > 57 && source[n] < 65) ||
-        (source[n] > 90 && source[n] < 97) || source[n] > 122) {
-      // Check if is alpha numerical
-      token.type = TK_UNKNOWN;
-      return token;
-    }
-  }
-
-  token.string = (char *)malloc(strlen(source));
-  strcpy(token.string, source);
-
-  return token;
-}
-
 void print_token(Token token) {
   if (token.type == TK_UNKNOWN && !strcmp(token.string, "")) {
     printf("_");
@@ -58,6 +24,48 @@ void print_tokens(Token *tokens, size_t token_count) {
   printf(" ]\n");
 }
 
+Token string_to_token(const char *source) {
+  Token token;
+  token.type = TK_UNKNOWN;
+
+  // Is the token an operator?
+  if (strchr(SINGLETS, source[0])) {
+    token.type = source[0];
+    token.string = (char *)malloc(2);
+    memset(token.string, 0, 2);
+    token.string[0] = source[0];
+    return token;
+  }
+
+  // Is the token a string?
+  if (source[0] == '"' && source[strlen(source)] == '"') {
+    token.type = TK_STRING;
+    token.string = (char *)malloc(strlen(source) - 1);
+    memset(token.string, 0, strlen(source));
+    strncpy(token.string, source + 1, strlen(source) - 1);
+    return token;
+  }
+
+  // Check if token is a valid symbol
+  for (int n = 0; n < strlen(source); n++) {
+    if (source[n] == ' ')
+      continue;
+    if (source[n] < 48 || (n == 0 && (source[n] < 58)) ||
+        (source[n] > 57 && source[n] < 65) ||
+        (source[n] > 90 && source[n] < 97) || source[n] > 122) {
+      token.type = TK_UNKNOWN;
+      return token;
+    }
+  }
+
+  token.type = TK_SYMBOL;
+  token.string = (char *)malloc(strlen(source));
+  memset(token.string, 0, strlen(source));
+  strcpy(token.string, source);
+
+  return token;
+}
+
 /// Source string should never contain new lines,
 /// tabs or more than one consecutive spaces.
 size_t scan_tokens(const char *source, Token *dest, size_t buffer_size) {
@@ -69,6 +77,11 @@ size_t scan_tokens(const char *source, Token *dest, size_t buffer_size) {
   bool is_string = false;
 
   for (int n = 0; n < strlen(source); n++) {
+    if (token_len == 1 && source[n] == ' ') {
+      token_start++;
+      continue;
+    }
+
     bool new_token = strchr(SPLIT_AT, source[n]) && !is_string;
 
     if (source[n] == '\"') {
@@ -81,10 +94,8 @@ size_t scan_tokens(const char *source, Token *dest, size_t buffer_size) {
       }
     }
 
-    if (new_token) { // TODO: Does not split at spaces, and
-                     // needs special case to handle strings
-      // use stringToToken and strncpy(result, str + start, end - start);
-      if (dest != NULL) {
+    if (new_token || n == strlen(source) - 1) {
+      if (dest != NULL && tokens < buffer_size) {
         char *buffer = (char *)malloc(token_len + 1);
         memset(buffer, 0, token_len + 1);
         strncpy(buffer, source + token_start, token_len);
--- tests/tokenizer.c
+++ tests/tokenizer.c
@@ -6,19 +6,27 @@
 
 int main() {
   // Symbols and Operators
-  if (scan_tokens("foo + bar", NULL, 0) != 3) {
-    printf("Expected 3 tokens in 'foo + bar'");
+  size_t token_count = scan_tokens("foo + bar", NULL, 0);
+  if (token_count != 3) {
+    printf("Expected 3 tokens in 'foo + bar', found %zu\n", token_count);
+
+    Token buffer[token_count];
+    scan_tokens("foo + bar", buffer, token_count);
+    printf(":\t");
+    print_tokens(buffer, token_count);
+
     return 1;
   }
 
   Token buffer[3];
   scan_tokens("foo + bar", buffer, 3);
 
+  // Test broken?
   if (buffer[0].string != "foo" || buffer[0].type != TK_SYMBOL ||
       buffer[1].type != TK_PLUS || buffer[1].string != "+" ||
       buffer[2].type != TK_SYMBOL || buffer[2].string != "bar") {
-    printf("Expected foo + bar\n");
-    printf("found: ");
+    printf("Assertion failed!\nExpected: foo + bar\n");
+    printf("Found: ");
     print_tokens(buffer, 3);
     return 1;
   }
