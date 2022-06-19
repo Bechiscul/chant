f985f8a310ddea5fa426350905807364fa27d714
unic0rn9k <unic0rn9k@protonmail.com>
2022-06-19 14:16:28 +0200

    first commit

---
+++ chantrs/target/debug/deps/chantrs-64cbdd96e3e51e20.d
@@ -0,0 +1,6 @@
+/home/unic0rn9k/Documents/chant/chantrs/target/debug/deps/chantrs-64cbdd96e3e51e20: src/main.rs src/parser.rs
+
+/home/unic0rn9k/Documents/chant/chantrs/target/debug/deps/chantrs-64cbdd96e3e51e20.d: src/main.rs src/parser.rs
+
+src/main.rs:
+src/parser.rs:
---
+++ chantrs/target/debug/deps/chantrs-bfade3a0b5a9e94e.d
@@ -0,0 +1,6 @@
+/home/unic0rn9k/Documents/chant/chantrs/target/debug/deps/chantrs-bfade3a0b5a9e94e: src/main.rs src/tokenizer.rs
+
+/home/unic0rn9k/Documents/chant/chantrs/target/debug/deps/chantrs-bfade3a0b5a9e94e.d: src/main.rs src/tokenizer.rs
+
+src/main.rs:
+src/tokenizer.rs:
